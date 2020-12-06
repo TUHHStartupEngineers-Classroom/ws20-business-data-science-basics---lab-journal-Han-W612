6c9728ecb3605c521dec36ab3384a76a61ac0954
Han-W612 <75266022+Han-W612@users.noreply.github.com>
2020-12-06 10:04:32 +0100

    Update Challenge_Chapter 4.R

--- Challenge_Chapter 4.R
+++ Challenge_Chapter 4.R
@@ -28,6 +28,8 @@ patent_tbl <- vroom(
   na         = c("", "NA", "NULL")
 )
 
+patent_tbl%>% glimpse()
+
 # 2.2 Assignee
 col_types_assignee <- list(
   id = col_character(),
@@ -44,6 +46,8 @@ assignee_tbl <- vroom(
   na         = c("", "NA", "NULL")
 )
 
+assignee_tbl%>% glimpse()
+
 # 2.3 Patent_assignee
 
 col_types_patent_assignee <- list(
@@ -58,6 +62,8 @@ patent_assignee_tbl <- vroom(
   col_types  = col_types_patent,
   na         = c("", "NA", "NULL")
 )
+
+patent_assignee_tbl%>% glimpse()
 
 # 2.4 USPC
 
@@ -76,9 +82,11 @@ uspc_tbl <- vroom(
   na         = c("", "NA", "NULL")
 ) 
 
+uspc_tbl%>% glimpse()
+
 # Set data.table
 
-setDT(patent_tbl)
+setDT(patent_tbl) #once I try to convert patent data to data.table, the Rstudio gets stuck.
 setDT(assignee_tbl)
 setDT(patent_assignee_tbl)
 setDT(uspc_tbl)
@@ -93,17 +101,50 @@ combined_data_1 <- merge(x = assignee_tbl, y = patent_assignee_tbl,
                           all.y = FALSE)
 toc()
 
-combined_data %>% glimpse()
+combined_data_1 %>% glimpse()
 
 setkey(combined_data_1, "type")
 key(combined_data_1)
 
 setorderv(combined_data_1, c("type", "organization"))
 
+combined_data_US<- combined_data_1[ (type == 2)]
+combined_data_US
 
+tic()
+top_10_US_companies <- combined_data_US[!is.na(organization), .N, by = organization]
+toc()
+setkey(top_10_US_companies, "organization")
+key(top_10_US_companies)
+setorderv(top_10_US_companies, c("N", "organization"), order = -1)
+as_tibble(top_10_US_companies, .rows = 10)
 
+### A tibble: 10 x 2
+### organization                                     N
+###<chr>                                        <int>
+###  1 International Business Machines Corporation 139092
+###2 General Electric Company                     47122
+###3 Intel Corporation                            42157
+###4 Hewlett-Packard Development Company, L.P.    35573
+###5 Microsoft Corporation                        30086
+###6 Micron Technology, Inc.                      28001
+###7 QUALCOMM Incorporated                        24703
+###8 Texas Instruments Incorporated               24182
+###9 Xerox Corporation                            23174
+###10 Apple Inc.                                   21821
 
 # Task2_Recent patent activity - top 10 US companies in 2019
+
+#once I try to convert patent data to data.table, the Rstudio gets stuck, the following code can not run as well.
+patent_2019_tbl<- patent_tbl[ lubridate::year(date) == "2019"]
+
+setnames(patent_2019_tb,"id","patent_id")
+
+combined_data_c2 <- merge(x = combined_data_c1, y = patent_2019_tb, 
+                          by    = "patent_id", 
+                          all.x = TRUE, 
+                          all.y = FALSE)
+
 
 # Task3_Innovation in Tech - top 5 USPTO tech main classess
 
